89d2abc2e33a3fb396fdb3d393d8d171acc14597
Thinkerfans <2656079093@qq.com>
2014-01-16 23:44:06 +0800

    add mystrlen2

--- mystrlen.c
+++ mystrlen.c
@@ -4,12 +4,20 @@ int mystrlen(const char * s){
 	if(NULL == s)
 		return 0;
 	char * tem = (char *) s;
+	while(*tem++);
+		//tem++;
+	return tem-s-1;
+}
+int mystrlen2(const char * s){
+	if(NULL == s)
+		return 0;
+	char * tem = (char *) s;
 	while(*tem)
 		tem++;
 	return tem-s;
 }
 int main(){
-	char * s = NULL;
-	printf("len=%d",strlen(s));
+	char * s = "12";
+	printf("len=%d",mystrlen(s));
 	
 }
