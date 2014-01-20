47f196a71173890f822e0d042b6bc47838b08293
Thinkerfans <2656079093@qq.com>
2014-01-20 22:08:22 +0800

    add strcmp function

---
+++ mystrcmp.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+
+char * mystrcmp(char * des,char * src){
+
+	char * tem = des;
+	while(*tem) tem++;
+	while(*tem++ = *src++);
+	return des;
+}
+
+
+int main(){
+	char s[30] = "hello";
+	char * s1 = "world";
+//	printf("%s\n",mystrcat(s,s1));
+	printf("%d\n",strcmp(s,s1));
+	return 0;
+}
